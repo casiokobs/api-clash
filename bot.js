035da142106343f38573d5fdde2f8267fac4bf4a
Casio <casiokobs12@gmail.com>
2022-07-27 13:27:13 -0300

    ajustes bot

--- bot.js
+++ bot.js
@@ -4,15 +4,24 @@ const client = new discord.Client();
 const axios = require("axios")
 client.login(token);
 client.on("ready", () => {
-  console.log("Bot is ready!!!");
+  axios.get('https://api.clashofclans.com/v1/clans?name=zuccheti',{headers:{Authorization: 'Bearer ' + clash_token }})
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
 });
 client.on("message", message => {
   if (!message.guild) return;
   if(message.channel.type == "dm") return
   if(message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-  const commandBody = message.content.slice(prefix.length);
+  const commandBody = message.content.slice(prefix.length).trim();
   const args  = commandBody.split(' ');
+  console.log(args);
   if (args.length == 0) {
     message.channel.send("Manoooooooooooooooooo, tu não deu nenhum comando filho da puta 👍 :flag_br:");
     return
@@ -22,7 +31,7 @@ client.on("message", message => {
     axios.get('https://api.clashofclans.com/v1/clans?name=zuccheti',{headers:{Authorization: 'Bearer ' + clash_token }})
     .then(function (response) {
       // handle success
-      console.log('dados'+response);
+      message.channel.send(response.data.items[0].name);
     })
     .catch(function (error) {
       // handle error
