a225fd2863593f459abec8ba60e4ae271a0e2909
Casio <casiokobs12@gmail.com>
2022-07-27 11:57:33 -0300

    alterada a versao do bot para a 12

--- bot.js
+++ bot.js
@@ -1,20 +1,35 @@
-const { Client,GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const client = new Client({ intents: [GatewayIntentBits.GuildMessages] });
-client.once('ready', () => {
-	console.log('Ready!');
-    console.log(GatewayIntentBits.GuildMessages);
-});
-client.once("message", (message) =>{ 
-    if(message.author.bot == true) return
-    // if(message.channel.type == "dm") return
-
-    if(message.content){
-        message.channel.send("Pong!")
-    }
-    if(message.content == "avatar"){
-    message.channel.send(`https://cdn.discordapp.com/avatars/518900443569717252/${message.author.avatar}.webp`)
-    }
-})
-// Login to Discord with your client's token
+const discord = require("discord.js");
+const { token, prefix, clash_token } = require('./config.json');
+const client = new discord.Client();
+const axios = require("axios")
 client.login(token);
+client.on("ready", () => {
+  console.log("Bot is ready!!!");
+});
+client.on("message", message => {
+  if (!message.guild) return;
+  if(message.channel.type == "dm") return
+  if(message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  const commandBody = message.content.slice(prefix.length);
+  const args  = commandBody.split(' ');
+  if (args.length == 0) {
+    message.channel.send("Manoooooooooooooooooo, tu não deu nenhum comando filho da puta 👍 :flag_br:");
+    return
+  }
+  if (args[0] === "help"){
+//   message.channel.send('tem q v ainda essa bosta👍 :flag_br:');
+    axios.get('https://api.clashofclans.com/v1/clans?name=zuccheti',{headers:{Authorization: 'Bearer ' + clash_token }})
+    .then(function (response) {
+      // handle success
+      console.log('dados'+response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    return;
+  }
+})
+
+
