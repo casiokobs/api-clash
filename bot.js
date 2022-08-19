a73394edd6e01eb0de0cf08451c38e062af3d2b6
Casio Kobs <42324716+casiokobs@users.noreply.github.com>
2022-08-19 10:01:36 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,19 @@ const client = new discord.Client();
 const axios = require("axios")
 client.login(token);
 client.on("ready", () => {
-  axios.get('https://api.clashofclans.com/v1/clans/%232QP0YPRUL/members?limit=50',{headers:{Authorization: 'Bearer ' + clash_token }})
+  axios.get('https://api.clashofclans.com/v1/clans/%232QP0YPRUL/currentwar',{headers:{Authorization: 'Bearer ' + clash_token }})
         .then(function (response) {
-          console.log(response.data);
+          console.log('bot on !!');
+          response.data.clan.members.forEach(members => {
+            if (members.attacks == undefined) {
+              members.name
+            }
+          });
         })
         .catch(function (error) {
-          // handle error
           console.log(error);
         })
         return;
-  console.log('bot on !!');
 });
 client.on("message", message => {
   if (!message.guild) return;
@@ -22,13 +25,7 @@ client.on("message", message => {
   if (!message.content.startsWith(prefix)) return;
   const commandBody = message.content.slice(prefix.length).trim();
   const args  = commandBody.split(' ');
-  console.log(args);
-  if (args.length == 0) {
-    message.channel.send("Manoooooooooooooooooo, tu não deu nenhum comando filho da puta 👍 :flag_br:");
-    return
-  }
   if (args[0] === "cla"){
-//   message.channel.send('tem q v ainda essa bosta👍 :flag_br:');
     axios.get('https://api.clashofclans.com/v1/clans?name=zuccheti',{headers:{Authorization: 'Bearer ' + clash_token }})
     .then(function (response) {
       message.channel.send(response.data.items[0].badgeUrls.medium)
@@ -40,13 +37,11 @@ client.on("message", message => {
       `);
     })
     .catch(function (error) {
-      // handle error
       console.log(error);
     })
     return;
   }
   if (args[0] === "war"){
-    //   message.channel.send('tem q v ainda essa bosta👍 :flag_br:');
         axios.get('https://api.clashofclans.com/v1/clans/%232QP0YPRUL/currentwar',{headers:{Authorization: 'Bearer ' + clash_token }})
         .then(function (response) {
           console.log(response.data);
@@ -56,9 +51,29 @@ client.on("message", message => {
             ${response.data.clan.destructionPercentage}   %     ${response.data.opponent.destructionPercentage}
             `)
             if(response.data.clan.stars>=response.data.opponent.stars && response.data.clan.destructionPercentage > response.data.opponent.destructionPercentage ){
-              message.channel.send(`COMEMOS O CU DELES :sunglasses: :sunglasses: :sunglasses: `)
+              message.channel.send(`https://tenor.com/view/clash-of-clans-gif-23752619`)
             }
-          } 
+          }
+          if(response.data.state =='notInWar'){
+            message.channel.send(`Ainda reina a paz, não estamos em guerra no momento\n`)
+            message.channel.send(`https://tenor.com/view/love-peace-friendship-amigos-bake-gif-25010735`)
+          }
+          if(response.data.state =='inWar') {
+            message.channel.send(`Essa guerra esta em andamento\n\n${response.data.clan.name}  🆚  ${response.data.opponent.name}\n
+            ${response.data.clan.stars}      ⭐     ${response.data.opponent.stars}
+            ${response.data.clan.destructionPercentage}    %     ${response.data.opponent.destructionPercentage}
+            `)
+            message.channel.send(`Esses cornos ainda nao atacaram:`)
+            
+            response.data.clan.members.forEach(members => {
+              if (members.attacks == undefined) {
+                message.channel.send(`:ox: ${members.name} :ox:`)
+              }
+            }); 
+          }
+          if(response.data.state =='preparation') {
+            message.channel.send(`Estamos em preparção, é melhor treinar suas tropas, e arrumar o layout de guerra\n\n${response.data.clan.name}  🆚  ${response.data.opponent.name}`)
+          }
         })
         .catch(function (error) {
           // handle error
@@ -69,14 +84,22 @@ client.on("message", message => {
   if (args[0] === "membros"){
     axios.get('https://api.clashofclans.com/v1/clans/%232QP0YPRUL/members?limit=50',{headers:{Authorization: 'Bearer ' + clash_token }})
     .then(function (response) {
-      console.log(response);  
-    })
+      const array = response.data['items'];
+      message.channel.send(
+        array.forEach(element => {
+          const texto = `${element.name}\nCargo: ${element.role}\nXP: ${element.expLevel}\nTrofeus: ${element.trophies}\nVila construtor:${element.versusTrophies}\n###############################`
+        })
+      )
+    })  
     .catch(function (error) {
       // handle error
       console.log(error);
     })
   }
-      
+  else{
+    message.channel.send("Comando Invalido");
+    message.channel.send("https://tenor.com/view/barbarian-clash-of-clan-gif-19248916");
+    return
+  }
+     
 })
-
-
